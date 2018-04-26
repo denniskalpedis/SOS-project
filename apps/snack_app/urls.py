4bee5f878d1ca9089c686b5fa05fc97f0698c5e6
unknown <morreydson@gmail.com>
2018-04-27 04:03:12 +0800

    changes made to views and urls

--- apps/snack_app/urls.py
+++ apps/snack_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.index),  
     #url(r'join/(?P<id>[0-9a-zA-Z% ]+)?/?$', views.join), 
     url(r'join/?$', views.joining),
-    url(r'new/?$', views.new), 
+    # url(r'new/?$', views.new), 
     url(r'create/?$', views.create), 
     url(r'group/(?P<id>[0-9a-zA-Z% ]+)?/?$', views.group), 
     url(r'upgrade_user/(?P<user_id>[0-9]+)/(?P<group_id>[0-9]+)/?$', views.upgrade_user),
--- apps/snack_app/views.py
+++ apps/snack_app/views.py
@@ -31,17 +31,17 @@ def index(request):
     
     return render(request, "sos/index.html", context)
 
-def new(request):
-    if "login" not in request.session:
-        redirect("/")
-    return render(request, "sos/create.html")
+# def new(request):
+#     if "login" not in request.session:
+#         redirect("/")
+#     return render(request, "sos/create.html")
 
 def create(request):
     errors = BuyGroup.objects.validate(request.POST)
     if len(errors):
         for error in errors:
             messages.error(request, error)
-        return redirect('sos/new')
+        return redirect('sos/join')
     current_user = Users.objects.get(id=request.session["login"])
     name = request.POST['name']
     password = request.POST['password']
@@ -49,33 +49,33 @@ def create(request):
     new.users.add(current_user)
     return redirect('/sos')
 
-def join(request, id = "None"):
-    current_user = Users.objects.get(id=request.session["login"])
-    group_buy = BuyGroup.objects.all().filter(name=id)
-    if "login" not in request.session:
-        redirect("/")
-    if request.method=="POST":
-        if request.POST["password"] == group_buy[0].password:
-            group_buy[0].users.add(current_user)
-            request.session['group'] = group_buy[0].id
-            return redirect('/')
-        return render(request, "sos/join.html")
-    else:
-        if id is None:
-            return redirect('/sos')
-        else:
-            if group_buy.count() < 1:
-                messages.error(request, "No Group Named {}!".format(id))
-                return redirect('/sos')
-            # if current_user in group_buy[0].users:
-            #     print "already a member"
-            #     return redirect('/sos')
-        context = {
-            "group":group_buy[0].name
-        }
-        return render(request, "sos/join.html", context)
+# def join(request, id = "None"):
+#     current_user = Users.objects.get(id=request.session["login"])
+#     group_buy = BuyGroup.objects.all().filter(name=id)
+#     if "login" not in request.session:
+#         redirect("/")
+#     if request.method=="POST":
+#         if request.POST["password"] == group_buy[0].password:
+#             group_buy[0].users.add(current_user)
+#             request.session['group'] = group_buy[0].id
+#             return redirect('/')
+#         return render(request, "sos/join.html")
+#     else:
+#         if id is None:
+#             return redirect('/sos')
+#         else:
+#             if group_buy.count() < 1:
+#                 messages.error(request, "No Group Named {}!".format(id))
+#                 return redirect('/sos')
+#             # if current_user in group_buy[0].users:
+#             #     print "already a member"
+#             #     return redirect('/sos')
+#         context = {
+#             "group":group_buy[0].name
+#         }
+#         return render(request, "sos/join.html", context)
 
-    return redirect('/sos')
+#     return redirect('/sos')
 
 def group(request, id):
     current_user = Users.objects.get(id=request.session["login"])
@@ -84,7 +84,7 @@ def group(request, id):
         redirect("/")
     if group_buy.count() < 1:
         messages.error(request, "No Group Named {}!".format(id))
-        return redirect('/sos')
+        return redirect('/sos/group.html')
     if group_buy[0].admin == current_user:
         userlevel = "admin"
     elif BuyGroup.objects.all().filter(tas=current_user).count() > 0:
